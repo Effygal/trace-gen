b61823d6aab2ea8bdcae4d5c6c0403916682f2df
yirongwn <yirongwn@pjd-rz>
2024-05-16 14:10:50 +0000

    add param for frequency-dependent level, freq sample from mixed addr space with ird sample.

--- src/trace_gen/TraceGenerator.py
+++ src/trace_gen/TraceGenerator.py
@@ -1,14 +1,14 @@
 """
 Trace Generator
 - Generate synthetic traces from a set of parameters;
-- Sampling IRT from the real trace;
+- Sampling ird from the real trace;
 Author: Peter Desnoyers & Yirong Wang
 Date: 04/23/2024
 """
 import numpy as np
 from trace_gen.misc import *
-import trace_gen.iad_wrapper as iad_wrapper
-from trace_gen.unroll import *
+# import trace_gen.iad_wrapper as iad_wrapper
+# from trace_gen.unroll import *
 import random
 
 class TraceGenerator:
@@ -17,41 +17,40 @@ class TraceGenerator:
         M: set size of items
         n: trace length
         freq_dist: item frequency distribution (zipfian, etc.)
-        irt_dist: inter-reference time distribution (hyperexponential, etc.)
+        ird_dist: inter-reference distance distribution (hyperexponential, etc.)
         assume zipfian, 0.2 frac are to 0.8 frac addrs.
         '''
         self.M = M
         self.n = n
         # self.items = np.arange(M)
-        self.params = None
+        self.weights = None
         # self.trace = None # define the trace here
                           # read/write ratio?
-        self.irt = []
+        self.ird = []
 
-    def assign_params(self, classes):
+    def assign_weights(self, classes):
         '''
-        Generate random parameters for the IRT distribution.
+        Generate random parameters for the ird distribution.
         '''
-        # num_classes random numbers between 0 and 1
-        random_numbers = [random.random() for _ in range(classes)]
-       
+        # Generate random numbers with a decreasing trend
+        random_numbers = [random.random() * (1 - i / classes) for i in range(classes)]
+    
         total = sum(random_numbers)
         
         # Normalize the numbers to make their sum 1
         normalized_numbers = np.array([x / total for x in random_numbers])
 
-        self.params = normalized_numbers
+        self.weights = normalized_numbers
         
         return normalized_numbers
     
-    def assign_params_with_skew(self, classes=5, skewness=3):
+    def assign_weights_with_skew(self, classes=5, skewness=3):
         '''
-        Generate random parameters for the IRT distribution with skewness;
-        Empirically we observe 5 classes and skewness of 3 works well.
+        Generate random parameters for the ird distribution with skewness.
         '''
 
-        # num_classes random numbers between 0 and 1
-        random_numbers = [random.random() for _ in range(classes)]
+        # Generate random numbers with a decreasing trend
+        random_numbers = [random.random() * (1 - i / classes) for i in range(classes)]
         
         # Scale the random numbers based on the skewness
         scaled_numbers = [x ** skewness for x in random_numbers]
@@ -60,37 +59,39 @@ class TraceGenerator:
         
         normalized_numbers = [x / total for x in scaled_numbers]
 
-        self.params = normalized_numbers
+        self.weights = normalized_numbers
         
         return normalized_numbers
     
-    def sample_irt(self):
+    def sample(self):
         '''
-        Sample irt from the set of different uniform distributions with rates = self.params.
+        Sample ird from the set of different uniform distributions with rates = self.weights.
         '''
-        num_intervals = len(self.params)
+        num_intervals = len(self.weights)
         
         # Calculate the width of each interval
         interval_width = (self.M) / num_intervals
 
         # Choose an interval based on the probabilities
-        choice = np.random.choice(num_intervals, p=self.params)
+        choice = np.random.choice(num_intervals, p=self.weights)
 
         # Calculate the lower and upper bounds of the chosen interval
         lower_bound = choice * interval_width
         upper_bound = (choice + 1) * interval_width
 
         # Sample from the chosen uniform interval
-        irt = np.random.uniform(lower_bound, upper_bound)  
-        self.irt.append(irt)
-        return int(irt)
+        ird = np.random.uniform(lower_bound, upper_bound)  
+        self.ird.append(ird)
+        return np.array(ird, dtype=np.int32)
 
-    def generate_trace(self, classes, skewness=0):
+    def generate_trace(self, classes, skewness=1, param=0):
         '''
         Generate a synthetic trace
+        param indicates the probability of treating the sample as a reference directly, i.e., to what extent the trace is frequency-based.
         '''
-        # self.assign_params_with_skew(classes, skewness)
-        self.assign_params(classes)
-        trace = gen_from_iad2(self.sample_irt, self.M, self.n)
+        self.assign_weights_with_skew(classes, skewness)
+
+        # trace = gen_from_ird2(self.sample, self.M, self.n)
+        trace = gen_from_both(self.sample, self.M, self.n, param)
         # self.trace = trace
         return trace
--- src/trace_gen/TraceReconstructor.py
+++ src/trace_gen/TraceReconstructor.py
@@ -1,10 +1,10 @@
 """
 Trace Reconstructor
 - Reconstructe synthetic traces from a (N*2) real trace;
-- Sampling IRT from the real trace;
-- Support IRM and IRT reconstruction from real traces;
-- "load_trace" function packages the original real trace into blocks before any processing;
-- retrieve only good IRTs from the real trace, eliminating noises from first encounters.
+- Sampling ird from the real trace;
+- Support IRM and ird reconstruction from real traces;
+- "pack_trace" function packages the original real trace into blocks before any processing;
+- retrieve only good irds from the real trace, eliminating noises from first encounters.
 
 Author: Peter Desnoyers & Yirong Wang
 Date: 04/23/2024
@@ -18,53 +18,50 @@ from trace_gen.unroll import *
 class TraceReconstructor:
     def __init__(self, trace):
         self.trace = trace
-        self.load_trace()
+        self.pack_trace()
         self.items, self.counts = np.unique(self.trace, return_counts=True)
         self.cdf = np.cumsum(self.counts) / len(self.trace)
         self.irm_trace = None
-        self.irt = None
+        self.ird = None
         self.M = np.sum(self.counts > 3)
-        self.irt_trace = None
+        self.ird_trace = None
         
-    def load_trace(self):
+    def pack_trace(self):
         self.trace[:, 0] += 7
         self.trace = squash(unroll(self.trace // 8))       
         
-    def calculate_cdf(self):
+    def get_cdf(self):
         self.items, self.counts = np.unique(self.trace, return_counts=True)
         self.cdf = np.cumsum(self.counts) / len(self.trace)
         
     def generate_irm_trace(self, length):
         if self.cdf is None:
-            self.calculate_cdf()
+            self.get_cdf()
         self.irm_trace = [np.searchsorted(self.cdf, _) for _ in np.random.random(length)]
         return self.irm_trace
 
-    # TODO: or use the following two functions to generate frequency trace, s.t we have a interface for sampling frequency.
-    def sample_freq(self):
-        return np.random.choice(self.items, p=self.counts/len(self.trace))
-    # def generate_freq_trace(self, length):
-    #     return gen_from_freq(self.sample_freq, self.M, length)
-
-    def retrieve_good_irts(self):
+    def get_irds(self):
+        if self.counts is None:
+            self.get_cdf()
         r_single = np.sum(self.counts == 1) / len(self.trace)
-        irt = iad_wrapper.iad(self.trace)
-        irt = irt[irt > 0]
-        n_single = int(r_single * len(irt))
-        self.irt = np.append(irt, np.ones(n_single)*-1)
+        ird = iad_wrapper.iad(self.trace)
+        ird = ird[ird > -1]
+        n_single = int(r_single * len(ird))
+        ird = np.append(ird, np.ones(n_single)*-1)
+        self.ird = ird
         
-    def sample_irt(self):
-        if self.irt is None:
-            self.retrieve_good_irts()
-        return np.random.choice(self.irt)
+    def sample_ird(self):
+        if self.ird is None:
+            self.get_irds()
+        return np.random.choice(self.ird)
         
-    def generate_irt_trace(self, length):
-        self.irt_trace = gen_from_iad2(self.sample_irt, self.M, length)
-        return self.irt_trace
+    def generate_ird_trace(self, length):
+        self.ird_trace = gen_from_ird2(self.sample_ird, self.M, length)
+        return self.ird_trace
     
     
 # Example usage:
 # trace_reconstructor = TraceReconstructor("your_trace_file_name")
 # 
 # irm_trace = trace_reconstructor.generate_irm_trace(length=10000)
-# irt_trace = trace_reconstructor.generate_irt_trace(length=10000)
+# ird_trace = trace_reconstructor.generate_ird_trace(length=10000)
